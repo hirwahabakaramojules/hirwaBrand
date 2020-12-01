1c256839795d4f3981f8a7304f7f13803a5e9b4e
hirwahabakaramojules <hirwahabakaramojules16@yahoo.com>
2020-12-01 19:13:30 +0100

    ft(retreive messages)get messages
    connect the admin dashboard and get messages
    
    [finishes kHxVcyHL]

--- UI/js/admin/admin.js
+++ UI/js/admin/admin.js
@@ -1,9 +1,23 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB25XlauyLSyrv6-FOq4scHxofKfuXzHhw",
+  authDomain: "hirwahjules-520b8.firebaseapp.com",
+  databaseURL: "https://hirwahjules-520b8.firebaseio.com",
+  projectId: "hirwahjules-520b8",
+  storageBucket: "hirwahjules-520b8.appspot.com",
+  messagingSenderId: "1014276523246",
+  appId: "1:1014276523246:web:1cd8452cec429ff05d9fff",
+  measurementId: "G-5PXYFR30WJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //selection of html ids
-
+const db = firebase.firestore()
 const accordion = document.getElementById("accordion");
 const panel = document.querySelector(".panel");
 const addUserMessage = document.querySelector("#addUserMessage");
 const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const usersTable = document.getElementById("usersTable");
+
 
 // functions
 
@@ -37,25 +51,32 @@ name.addEventListener("blur", () => {
     name.style.border = "2px solid rgba(100, 189, 85, 0.9)";
   }
 });
+const renderMsg = (doc) =>{
+  let tr = document.createElement("tr");
+  let name = document.createElement("td");
+  let email = document.createElement("td");
+  let msg = document.createElement("td");
 
-accordion.addEventListener("click", () => {
-  // console.log("btn clicked");
-  if (panel.style.display === "" || panel.style.display === "none") {
-    panel.style.display = "block";
-    // console.log("panel is now opened");
-  } else {
-    panel.style.display = "none";
-  }
-});
+  tr.setAttribute( "id", doc.id)
+  name.textContent = doc.data().name;
+  email.textContent = doc.data().email;
+  msg.textContent = doc.data().message;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!email.value || !name.value) {
-    addUserMessage.innerHTML = "please fill in all the information";
-    addUserMessage.style.display = "block";
-    email.style.border = "2px red solid";
-    name.style.border = "2px red solid";
-  }
+  tr.appendChild(name);
+  tr.appendChild(email);
+  tr.appendChild(msg);
 
-  console.log("submitted");
-});
+  usersTable.appendChild(tr);
+}
+
+// get messages
+db.collection("messages").onSnapshot(snapshot =>{
+    let changes = snapshot.docChanges();
+    //
+    changes.forEach(change =>{      
+      if(change.type === "added"){
+        renderMsg(change.doc);
+      }
+    })
+   
+})
